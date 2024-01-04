39232f5c140a45ed79c0be61c5f20f17cb374235
alexey0101 <timbersaw123987@gmail.com>
2024-01-04 16:24:08 +0300

    test

--- src/main/java/com/karyakin/PasswordGenerator/controller/DevPasswordGeneratorController.java
+++ src/main/java/com/karyakin/PasswordGenerator/controller/DevPasswordGeneratorController.java
@@ -7,13 +7,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/password/")
+@RequestMapping("/dev/api/v1/password/")
 @RequiredArgsConstructor
-@Profile("!dev")
-public class PasswordGeneratorController {
+@Profile("dev")
+public class DevPasswordGeneratorController {
 
     private final RandomPasswordGenerator randomPasswordGenerator;
 
